8c60aeb7148f43f282846cb90761ba48f160f4d4
madejean <madejean007@hotmail.fr>
2017-06-13 12:40:32 -0700

    more comments

--- 0x01-python_basics/0-print_alphabt.py
+++ 0x01-python_basics/0-print_alphabt.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-""" Prints the entire alphabet except for the letters q and e """
+
+"""Prints the entire alphabet except for the letters q and e """
+
 for i in range(ord("a"), ord("z")):
+    """iterates from a to z not printing q and e"""
     if i != ord("q") and i != ord("e"):
+        """does not print a new line"""
         print("{}".format(chr(i)), end="")
