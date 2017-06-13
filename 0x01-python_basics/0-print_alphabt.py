383ff444dc5073117e5c743c829107aa092ee7c6
madejean <madejean007@hotmail.fr>
2017-06-13 12:47:53 -0700

    iterating over ascii characters

--- 0x01-python_basics/0-print_alphabt.py
+++ 0x01-python_basics/0-print_alphabt.py
@@ -2,8 +2,6 @@
 
 """Prints the entire alphabet except for the letters q and e """
 
-for i in range(ord("a"), ord("z")):
-    """iterates from a to z not printing q and e"""
-    if i != ord("q") and i != ord("e"):
-        """does not print a new line"""
+for i in range(97, 122):
+    if i != 113 and i != 101:
         print("{}".format(chr(i)), end="")
