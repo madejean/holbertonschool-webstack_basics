7ea658fc84d7f1fe9d95c21828d2a3aa9449695a
madejean <madejean007@hotmail.fr>
2017-06-13 12:35:30 -0700

    added comment

--- 0x01-python_basics/0-print_alphabt.py
+++ 0x01-python_basics/0-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" Prints the entire alphabet except for the letters q and e """
 for i in range(ord("a"), ord("z")):
     if i != ord("q") and i != ord("e"):
         print("{}".format(chr(i)), end="")
