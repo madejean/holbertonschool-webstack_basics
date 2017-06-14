b06aac9d92c5b8ce25e66f9dd8fba136a7dcd398
madejean <madejean007@hotmail.fr>
2017-06-14 10:18:07 -0700

    changed function to not execute when importing

--- 0x01-python_basics/4-add.py
+++ 0x01-python_basics/4-add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 add = __import__("add_4").add
 
-a = 1
-b = 2
+def addition():
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(1, 2)))
 
-print("{} + {} = {}".format(a , b, add(1, 2)))
+"""not executing when imported"""
+if __name__ == "__main__":
+    addition()
