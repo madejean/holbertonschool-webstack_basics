9776b5a71c65f1dd765c97e13ba58c837409bc9f
madejean <madejean007@hotmail.fr>
2017-06-14 11:17:23 -0700

    running checkers

--- 0x01-python_basics/5-args.py
+++ 0x01-python_basics/5-args.py
@@ -5,16 +5,16 @@ import sys
 def number_agrv():
     """outputs the number of argmuments passed from command-line"""
     argv = sys.argv[1:]
-
-    if len(argv) == 0:
-        print("{} argmument.".format(len(argv)))
+    len_argv = len(argv)
+    if len_argv == 0:
+        print("{} argmument.".format(len_argv))
     elif len(argv) == 1:
-        print("{} argmument:".format(len(argv)))
+        print("{} argmument:".format(len_argv))
         for i in argv:
-            print("{}: {}".format(len(argv), i))
+            print("{}: {}".format(len_argv, i))
     else:
-        print("{} argmuments:".format(len(argv)))
-        for i in range(len(argv)):
+        print("{} argmuments:".format(len_argv))
+        for i in range(len_argv):
             print("{}: {}".format(i + 1, argv[i]))
 
 if __name__ == "__main__":
