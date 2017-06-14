f8605d0ec8091639cb8dd52a6e6f701600fcc410
madejean <madejean007@hotmail.fr>
2017-06-14 11:30:50 -0700

    comment

--- 0x01-python_basics/5-args.py
+++ 0x01-python_basics/5-args.py
@@ -5,16 +5,15 @@ import sys
 def number_agrv():
     """outputs the number of argmuments passed from command-line"""
     argv = sys.argv[1:]
-    len_argv = len(argv)
-    if len_argv == 0:
-        print("{} argmument.".format(len_argv))
+    if len(argv) == 0:
+        print("{} argmument.".format(len(argv)))
     elif len(argv) == 1:
-        print("{} argmument:".format(len_argv))
+        print("{} argmument:".format(len(argv)))
         for i in argv:
-            print("{}: {}".format(len_argv, i))
+            print("{}: {}".format(len(argv), i))
     else:
-        print("{} argmuments:".format(len_argv))
-        for i in range(len_argv):
+        print("{} argmuments:".format(len(argv)))
+        for i in range(len(argv)):
             print("{}: {}".format(i + 1, argv[i]))
 
 if __name__ == "__main__":
