1f70b79bbd308edaa1eae23744b376aeea8a05cc
madejean <madejean007@hotmail.fr>
2017-06-14 11:08:47 -0700

    added multiline comment

--- 0x01-python_basics/7-max_integer.py
+++ 0x01-python_basics/7-max_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    """returns the max integer from the list"""
+    """
+    returns the max integer from the list
+    """
     num = my_list[0]
     for i in my_list:
         if num < i:
