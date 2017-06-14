419643c95f62f9daeb845d03e6df7f8881c85571
madejean <madejean007@hotmail.fr>
2017-06-14 11:41:51 -0700

    added pep8 style

--- 0x01-python_basics/14-singly_linked_list.py
+++ 0x01-python_basics/14-singly_linked_list.py
@@ -22,10 +22,10 @@ class Node:
 
     @next_node.setter
     def next_node(self, value):
-        if value is not None and not isinstance(value, Node):
-            raise TypeError("next_node must be a Node object")
-        else:
+        if value is None or isinstance(value, Node):
             self.__next_node = value
+        else:
+            raise TypeError("next_node must be a Node object")
 
 
 class SinglyLinkedList:
