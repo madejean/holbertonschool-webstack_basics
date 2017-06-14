7b4cf09ac95f92d2a393efe3da46e4a2f4ffd0b0
madejean <madejean007@hotmail.fr>
2017-06-14 10:24:32 -0700

    added comments

--- 0x01-python_basics/4-add.py
+++ 0x01-python_basics/4-add.py
@@ -3,9 +3,11 @@ add = __import__("add_4").add
 
 
 def addition():
+    """calls the imported add function passing it the value of a and b"""
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(1, 2)))
 
+"""not executing when imported"""
 if __name__ == "__main__":
     addition()
