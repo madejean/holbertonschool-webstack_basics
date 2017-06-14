04c49120ed8e3a082b557fe4b1afe2c43c3a073a
madejean <madejean007@hotmail.fr>
2017-06-14 11:13:05 -0700

    added condition to check empty list

--- 0x01-python_basics/7-max_integer.py
+++ 0x01-python_basics/7-max_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    num = my_list[0]
-    for i in my_list:
-        if num < i:
-            num = i
+    """returns the max integer from the list"""
+    if not my_list:
+        return None
+    else:
+        num = my_list[0]
+        for i in my_list:
+            if num < i:
+                num = i
     return num
