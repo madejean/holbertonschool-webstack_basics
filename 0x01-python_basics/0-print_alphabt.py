5e58c241c905ffd7b948aabc6dd03cd100b4bbd7
madejean <madejean007@hotmail.fr>
2017-06-14 08:54:39 -0700

    removed spaces

--- 0x01-python_basics/0-print_alphabt.py
+++ 0x01-python_basics/0-print_alphabt.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
 """Prints the entire alphabet except for the letters q and e"""
-
 for i in range(ord("a"), ord("z") + 1):
     if i != ord("q") and i != ord("e"):
         print("{}".format(chr(i)), end="")
