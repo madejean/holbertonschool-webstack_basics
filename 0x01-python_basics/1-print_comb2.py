c77be849b26ed159aa50414d092d185fbe089864
madejean <madejean007@hotmail.fr>
2017-06-15 10:50:57 -0700

    running checkers

--- 0x01-python_basics/1-print_comb2.py
+++ 0x01-python_basics/1-print_comb2.py
@@ -2,5 +2,4 @@
 for i in range(0, 100):
     if i < 9:
         print("{}".format(0), end="")
-    print("{}".format(i), end=", "
-            if i <= 98 else "\n")
+    print("{}".format(i), end=", " if i <= 98 else "\n")
