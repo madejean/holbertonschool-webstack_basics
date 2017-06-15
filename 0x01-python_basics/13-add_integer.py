d34f4eb9b9f2ae4c1c38f8a88b3cfbfbe1db841f
madejean <madejean007@hotmail.fr>
2017-06-15 12:17:23 -0700

    running checkers

--- 0x01-python_basics/13-add_integer.py
+++ 0x01-python_basics/13-add_integer.py
@@ -13,7 +13,7 @@ def add_integer(a, b):
     """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    elif not isinstance(b, int) and not isinstance(b, float):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
     else:
         return (int(a) + int(b))
