9b6b63c56f1dc0d2bde663d5428e63a157ee9972
madejean <madejean007@hotmail.fr>
2017-06-15 11:11:06 -0700

    runnning checkers

--- 0x01-python_basics/4-add.py
+++ 0x01-python_basics/4-add.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-add = __import__("add_4").add
+ad = __import__("add_4").add
 
 
 def addition():
-    """calls the imported add function passing it the value of a and b"""
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(1, 2)))
+    print("{} + {} = {}".format(a, b, ad(1, 2)))
 
-"""not executing when imported"""
 if __name__ == "__main__":
     addition()
