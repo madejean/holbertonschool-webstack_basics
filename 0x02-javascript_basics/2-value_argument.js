558274fe0e7778edb9767283d4ca9972a9996eb4
madejean <madejean007@hotmail.fr>
2017-06-21 09:27:45 -0700

    first commit

---
+++ 0x02-javascript_basics/2-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+let i = 0;
+
+while (process.argv[i]) {
+  i++;
+}
+if (i <= 2) {
+  console.log('No argument');
+} else {
+    console.log(process.argv[2]);
+}
---
+++ 0x02-javascript_basics/3-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+let input = process.argv[2]
+
+if (isNaN(input)) {
+  console.log("Not a number")
+}
+else {
+  console.log("My number:", parseInt(Number(input), 10))
+}
