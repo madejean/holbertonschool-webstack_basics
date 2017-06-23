69de45c5775f6e1d748b255902759c04da06d4b7
madejean <madejean007@hotmail.fr>
2017-06-22 21:42:53 -0700

    running checkers

--- 0x02-javascript_basics/3-to_integer.js
+++ 0x02-javascript_basics/3-to_integer.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-let input = process.argv;
-let argument = input[2];
+let input = process.argv[2];
 
 if (isNaN(input)) {
   console.log('Not a number');
 } else {
-  console.log('My number:', input);
+  console.log('My number:' + input);
 }
