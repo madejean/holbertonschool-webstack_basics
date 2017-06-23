d1775c755d7876367153940c4dda4108478f338e
madejean <madejean007@hotmail.fr>
2017-06-22 21:44:15 -0700

    running checkers

--- 0x02-javascript_basics/3-to_integer.js
+++ 0x02-javascript_basics/3-to_integer.js
@@ -4,5 +4,5 @@ let input = process.argv[2];
 if (isNaN(input)) {
   console.log('Not a number');
 } else {
-  console.log('My number:' + input);
+  console.log('My number:', input);
 }
--- 0x02-javascript_basics/4-square.js
+++ 0x02-javascript_basics/4-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 let size = process.argv[2];
 
-if (Number.isNaN(size)) {
+if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
