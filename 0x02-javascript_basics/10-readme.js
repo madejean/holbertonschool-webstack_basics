ea4b9ae54723d608d2f5b51e56f1ec0f98588ffb
madejean <madejean007@hotmail.fr>
2017-06-22 21:25:48 -0700

    running checkers

--- 0x02-javascript_basics/10-readme.js
+++ 0x02-javascript_basics/10-readme.js
@@ -4,7 +4,7 @@ let file = process.argv[2];
 
 fs.readFile(file, 'utf8', function (err, data) {
   if (err) {
-    return console.log(err);
+    console.log(err);
   }
   process.stdout.write(data);
 });
