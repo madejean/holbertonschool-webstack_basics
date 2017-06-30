760e96dcc41dba754983e835a117a1b9aa2c5c65
madejean <madejean007@hotmail.fr>
2017-06-30 11:10:01 -0700

    running checkers

--- 0x03-python_web_scraping/8-starwars.py
+++ 0x03-python_web_scraping/8-starwars.py
@@ -3,21 +3,21 @@ import sys
 import requests
 
 
-def get_people():
-    params = sys.argv[1]
-    r = requests.get('http://swapi.co/api/people/', params={'search': params})
-    j = r.json()
-    results = j['results']
+def get_people(j, results):
 
-    print("Number of result: {}".format(j['count']))
     for i in results:
         print(i['name'])
 
     if j['next'] is not None:
         n = requests.get(j['next']).json()
         next_results = n['results']
-        for i in next_results:
-            print(i['name'])
+        get_people(n, next_results)
 
 if __name__ == "__main__":
-    get_people()
+    params = sys.argv[1]
+    r = requests.get('http://swapi.co/api/people/', params={'search': params})
+    j = r.json()
+    results = j['results']
+
+    print("Number of result: {}".format(j['count']))
+    get_people(j, results)
