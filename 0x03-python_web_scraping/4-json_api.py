1a171fa10b94a2b29fc1066afeab03e31ccce937
madejean <madejean007@hotmail.fr>
2017-06-30 10:46:56 -0700

    running checkers

--- 0x03-python_web_scraping/4-json_api.py
+++ 0x03-python_web_scraping/4-json_api.py
@@ -4,19 +4,20 @@ import requests
 
 
 def search_user():
-    q = ''
-
     if len(sys.argv) > 1:
         q = sys.argv[1]
+    else:
+        q = ''
 
     r = requests.post('http://54.145.119.185:33424/search_user', data={'q': q})
 
     try:
         result = r.json()
-        if result == {}:
+        if len(result) == 0:
             print("No result")
         else:
             print([result["id"]], result["name"])
+
 
     except:
         print("Not a valid JSON")
