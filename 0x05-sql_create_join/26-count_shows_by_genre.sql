55915a02f7b20a0dbb5240c053971ef1fe769de5
madejean <madejean007@hotmail.fr>
2017-07-21 11:17:18 -0700

    running checkers

--- 0x05-sql_create_join/26-count_shows_by_genre.sql
+++ 0x05-sql_create_join/26-count_shows_by_genre.sql
@@ -2,4 +2,4 @@
 SELECT tv_genres.name AS genre, COUNT(tv_genres.name) AS number_shows FROM tv_genres
 LEFT JOIN tv_show_genres on tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
-ORDER BY number_shows DESC, genre;
+ORDER BY COUNT(tv_genres.name) DESC;
