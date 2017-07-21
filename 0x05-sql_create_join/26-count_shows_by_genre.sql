9533bf07304405c6d1e45a0e1f7ecc915e79778d
madejean <madejean007@hotmail.fr>
2017-07-21 11:12:35 -0700

    running checkers

--- 0x05-sql_create_join/26-count_shows_by_genre.sql
+++ 0x05-sql_create_join/26-count_shows_by_genre.sql
@@ -1,5 +1,7 @@
 -- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each other
-SELECT tv_genres.name AS genre, COUNT(tv_genres.name) AS number_shows FROM tv_genres
-LEFT JOIN tv_show_genres on tv_genres.id = tv_show_genres.genre_id
+SELECT tv_genres.name AS genre,
+COUNT(tv_genres.name) AS number_shows
+FROM tv_genres LEFT JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
 ORDER BY COUNT(tv_genres.name) DESC;
